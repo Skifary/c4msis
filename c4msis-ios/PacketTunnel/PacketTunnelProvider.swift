56535c7d7afd7256ea2da19184044ff364616386
Skifary <376512563@qq.com>
2017-12-29 19:56:25 +0800

    rewrite PacketTunnelProvider

--- c4msis-ios/PacketTunnel/PacketTunnelProvider.swift
+++ c4msis-ios/PacketTunnel/PacketTunnelProvider.swift
@@ -11,77 +11,138 @@ import NEKit
 import CocoaLumberjackSwift
 import Yaml
 
+typealias SSConfig = (address: String, port: Int, method: String, password: String, yamlString: String)
+
 class PacketTunnelProvider: NEPacketTunnelProvider {
     
     var interface: TUNInterface!
-    var enablePacketProcessing = false
     
-    var proxyPort: Int!
+    var proxyPort: Int = 9090
     
     var proxyServer: ProxyServer!
     
     var lastPath:NWPath?
-    
-    var started:Bool = false
-    
+
     override func startTunnel(options: [String : NSObject]?, completionHandler: @escaping (Error?) -> Void) {
         DDLog.removeAllLoggers()
         DDLog.add(DDASLLogger.sharedInstance, with: DDLogLevel.info)
         ObserverFactory.currentFactory = DebugObserverFactory()
         NSLog("-------------")
         
-        guard let conf = (protocolConfiguration as! NETunnelProviderProtocol).providerConfiguration else{
+        guard let config = (protocolConfiguration as! NETunnelProviderProtocol).providerConfiguration else{
             NSLog("[ERROR] No ProtocolConfiguration Found")
             exit(EXIT_FAILURE)
         }
         
+        guard let networkSettings = networkSettings(from: config)  else {
+            NSLog("[ERROR] NetworkSettings Is Nil")
+            exit(EXIT_FAILURE)
+        }
         
-        let ss_adder = conf["ss_address"] as! String
-        NSLog(ss_adder)
+        setTunnelNetworkSettings(networkSettings) { error in
+            guard error == nil else {
+                DDLogError("Encountered an error setting up the network: \(error.debugDescription)")
+                completionHandler(error)
+                return
+            }
+            
+            self.proxyServer = GCDHTTPProxyServer(address: IPAddress(fromString: "127.0.0.1"), port: NEKit.Port(port: UInt16(self.proxyPort)))
+            try! self.proxyServer.start()
+            
+            completionHandler(nil)
+        }
         
-        let ss_port = conf["ss_port"] as! Int
-        let method = conf["ss_method"] as! String
-        NSLog(method)
+    }
+    
+    override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
+
+        if(proxyServer != nil){
+            proxyServer.stop()
+            proxyServer = nil
+            RawSocketFactory.TunnelProvider = nil
+        }
         
-        let password = conf["ss_password"] as!String
+        completionHandler()
+        exit(EXIT_SUCCESS)
+    }
+    
+}
+
+extension PacketTunnelProvider {
+    
+    func check(config: [String : Any]) -> SSConfig? {
         
-        // Proxy Adapter
+        guard let ssAddress = config["ss_address"] as? String  else {
+            NSLog("[ERROR] Address Is Nil")
+            return nil
+        }
         
+        guard let ssPort = config["ss_port"] as? Int  else {
+            NSLog("[ERROR] Address Is Nil")
+            return nil
+        }
         
-        // SSR Httpsimple
-        //        let obfuscater = ShadowsocksAdapter.ProtocolObfuscater.HTTPProtocolObfuscater.Factory(hosts:["intl.aliyun.com","cdn.aliyun.com"], customHeader:nil)
+        guard let ssMethod = config["ss_method"] as? String  else {
+            NSLog("[ERROR] Method Is Nil")
+            return nil
+        }
         
+        guard let ssPassword = config["ss_password"] as? String  else {
+            NSLog("[ERROR] Password Is Nil")
+            return nil
+        }
         
-        // Origin
-        let obfuscater = ShadowsocksAdapter.ProtocolObfuscater.OriginProtocolObfuscater.Factory()
-        
-        
-        let algorithm:CryptoAlgorithm
-        
-        switch method{
-        case "AES-128-CFB":algorithm = .AES128CFB
-        case "AES-192-CFB":algorithm = .AES192CFB
-        case "AES-256-CFB":algorithm = .AES256CFB
-        case "CHACHA20":algorithm = .CHACHA20
-        case "SALSA20":algorithm = .SALSA20
-        case "RC4-MD5":algorithm = .RC4MD5
+        guard let ssYAMLStr = config["ymal_conf"] as? String  else {
+            NSLog("[ERROR] YAML Is Nil")
+            return nil
+        }
+    
+        return (ssAddress, ssPort, ssMethod, ssPassword, ssYAMLStr);
+    }
+    
+    func shadowsocksAdapterFactory(from ssConfig: SSConfig) -> ShadowsocksAdapterFactory {
+    
+        let algorithm: CryptoAlgorithm
+        switch ssConfig.method {
+        case "AES-128-CFB":
+            algorithm = .AES128CFB
+        case "AES-192-CFB":
+            algorithm = .AES192CFB
+        case "AES-256-CFB":
+            algorithm = .AES256CFB
+        case "CHACHA20":
+            algorithm = .CHACHA20
+        case "SALSA20":
+            algorithm = .SALSA20
+        case "RC4-MD5":
+            algorithm = .RC4MD5
         default:
             fatalError("Undefined algorithm!")
         }
         
+        let obfuscater = ShadowsocksAdapter.ProtocolObfuscater.OriginProtocolObfuscater.Factory()
+        let cryptorFactory =  ShadowsocksAdapter.CryptoStreamProcessor.Factory(password: ssConfig.password, algorithm: algorithm)
+        let streamObfuscaterFactory = ShadowsocksAdapter.StreamObfuscater.OriginStreamObfuscater.Factory()
         
-        let ssAdapterFactory = ShadowsocksAdapterFactory(serverHost: ss_adder, serverPort: ss_port, protocolObfuscaterFactory:obfuscater, cryptorFactory: ShadowsocksAdapter.CryptoStreamProcessor.Factory(password: password, algorithm: algorithm), streamObfuscaterFactory: ShadowsocksAdapter.StreamObfuscater.OriginStreamObfuscater.Factory())
+        let ssAdapterFactory = ShadowsocksAdapterFactory(serverHost: ssConfig.address, serverPort: ssConfig.port, protocolObfuscaterFactory:obfuscater, cryptorFactory: cryptorFactory, streamObfuscaterFactory: streamObfuscaterFactory)
         
+        return ssAdapterFactory
+    }
+    
+    func setCurrentRules(_ ssConfig: SSConfig) {
+        
+        let ssAdapterFactory = shadowsocksAdapterFactory(from: ssConfig)
         let directAdapterFactory = DirectAdapterFactory()
         
         //Get lists from conf
-        let yaml_str = conf["ymal_conf"] as!String
-        let value = try! Yaml.load(yaml_str)
+        let yamlString = ssConfig.yamlString
+        let yamlValue = try! Yaml.load(yamlString)
         
         var UserRules:[NEKit.Rule] = []
         
-        for each in (value["rule"].array! ){
-            let adapter:NEKit.AdapterFactory
+        for each in (yamlValue["rule"].array!) {
+            
+            let adapter: NEKit.AdapterFactory
             if each["adapter"].string! == "direct"{
                 adapter = directAdapterFactory
             }else{
@@ -91,28 +152,20 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
             let ruleType = each["type"].string!
             switch ruleType {
             case "domainlist":
-                var rule_array : [NEKit.DomainListRule.MatchCriterion] = []
+                var ruleArray: [NEKit.DomainListRule.MatchCriterion] = []
                 for dom in each["criteria"].array!{
-                    let raw_dom = dom.string!
-                    let index = raw_dom.index(raw_dom.startIndex, offsetBy: 1)
-                    let index2 = raw_dom.index(raw_dom.startIndex, offsetBy: 2)
-                    let typeStr = raw_dom.substring(to: index)
-                    let url = raw_dom.substring(from: index2)
-                    
-                    if typeStr == "s"{
-                        rule_array.append(DomainListRule.MatchCriterion.suffix(url))
-                    }else if typeStr == "k"{
-                        rule_array.append(DomainListRule.MatchCriterion.keyword(url))
-                    }else if typeStr == "p"{
-                        rule_array.append(DomainListRule.MatchCriterion.prefix(url))
-                    }else if typeStr == "r"{
-                        // ToDo:
-                        // shoud be complete
+                    let rawDom = dom.string?.split(separator: ",")
+                    let type = rawDom?[0]
+                    let url = String(describing: rawDom?[1])
+                    if type == "s"{
+                        ruleArray.append(DomainListRule.MatchCriterion.suffix(url))
+                    }else if type == "k"{
+                        ruleArray.append(DomainListRule.MatchCriterion.keyword(url))
+                    }else if type == "p"{
+                        ruleArray.append(DomainListRule.MatchCriterion.prefix(url))
                     }
                 }
-                UserRules.append(DomainListRule(adapterFactory: adapter, criteria: rule_array))
-                
-                
+                UserRules.append(DomainListRule(adapterFactory: adapter, criteria: ruleArray))
             case "iplist":
                 let ipArray = each["criteria"].array!.map{$0.string!}
                 UserRules.append(try! IPRangeListRule(adapterFactory: adapter, ranges: ipArray))
@@ -121,43 +174,20 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
             }
         }
         
-        
         // Rules
-        
         let chinaRule = CountryRule(countryCode: "CN", match: true, adapterFactory: directAdapterFactory)
-        let unKnowLoc = CountryRule(countryCode: "--", match: true, adapterFactory: directAdapterFactory)
+        let unknownLoc = CountryRule(countryCode: "--", match: true, adapterFactory: directAdapterFactory)
         let dnsFailRule = DNSFailRule(adapterFactory: ssAdapterFactory)
         
         let allRule = AllRule(adapterFactory: ssAdapterFactory)
-        UserRules.append(contentsOf: [chinaRule,unKnowLoc,dnsFailRule,allRule])
+        UserRules.append(contentsOf: [chinaRule, unknownLoc, dnsFailRule, allRule])
         
         let manager = RuleManager(fromRules: UserRules, appendDirect: true)
         
         RuleManager.currentManager = manager
-        proxyPort =  9090
-        
-        //        RawSocketFactory.TunnelProvider = self
-        
-        // the `tunnelRemoteAddress` is meaningless because we are not creating a tunnel.
-        let networkSettings = NEPacketTunnelNetworkSettings(tunnelRemoteAddress: "8.8.8.8")
-        networkSettings.mtu = 1500
-        
-        let ipv4Settings = NEIPv4Settings(addresses: ["192.169.89.1"], subnetMasks: ["255.255.255.0"])
-        if enablePacketProcessing {
-            ipv4Settings.includedRoutes = [NEIPv4Route.default()]
-            ipv4Settings.excludedRoutes = [
-                NEIPv4Route(destinationAddress: "10.0.0.0", subnetMask: "255.0.0.0"),
-                NEIPv4Route(destinationAddress: "100.64.0.0", subnetMask: "255.192.0.0"),
-                NEIPv4Route(destinationAddress: "127.0.0.0", subnetMask: "255.0.0.0"),
-                NEIPv4Route(destinationAddress: "169.254.0.0", subnetMask: "255.255.0.0"),
-                NEIPv4Route(destinationAddress: "172.16.0.0", subnetMask: "255.240.0.0"),
-                NEIPv4Route(destinationAddress: "192.168.0.0", subnetMask: "255.255.0.0"),
-                NEIPv4Route(destinationAddress: "17.0.0.0", subnetMask: "255.0.0.0"),
-                
-            ]
-        }
-        networkSettings.ipv4Settings = ipv4Settings
-        
+    }
+    
+    func proxySettings() -> NEProxySettings {
         let proxySettings = NEProxySettings()
         proxySettings.httpEnabled = true
         proxySettings.httpServer = NEProxyServer(address: "127.0.0.1", port: proxyPort)
@@ -167,108 +197,28 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
         // This will match all domains
         proxySettings.matchDomains = [""]
         proxySettings.exceptionList = ["api.smoot.apple.com","configuration.apple.com","xp.apple.com","smp-device-content.apple.com","guzzoni.apple.com","captive.apple.com","*.ess.apple.com","*.push.apple.com","*.push-apple.com.akadns.net"]
-        networkSettings.proxySettings = proxySettings
         
-        if enablePacketProcessing {
-            let DNSSettings = NEDNSSettings(servers: ["198.18.0.1"])
-            DNSSettings.matchDomains = [""]
-            DNSSettings.matchDomainsNoSearch = false
-            networkSettings.dnsSettings = DNSSettings
-        }
-        
-        setTunnelNetworkSettings(networkSettings) {
-            error in
-            guard error == nil else {
-                DDLogError("Encountered an error setting up the network: \(error.debugDescription)")
-                completionHandler(error)
-                return
-            }
-            
-            
-            if !self.started{
-                self.proxyServer = GCDHTTPProxyServer(address: IPAddress(fromString: "127.0.0.1"), port: NEKit.Port(port: UInt16(self.proxyPort)))
-                try! self.proxyServer.start()
-                self.addObserver(self, forKeyPath: "defaultPath", options: .initial, context: nil)
-            }else{
-                self.proxyServer.stop()
-                try! self.proxyServer.start()
-            }
-            
-            completionHandler(nil)
-            
-            
-            if self.enablePacketProcessing {
-                if self.started{
-                    self.interface.stop()
-                }
-                
-                self.interface = TUNInterface(packetFlow: self.packetFlow)
-                
-                
-                let fakeIPPool = try! IPPool(range: IPRange(startIP: IPAddress(fromString: "198.18.1.1")!, endIP: IPAddress(fromString: "198.18.255.255")!))
-                
-                
-                let dnsServer = DNSServer(address: IPAddress(fromString: "198.18.0.1")!, port: NEKit.Port(port: 53), fakeIPPool: fakeIPPool)
-                let resolver = UDPDNSResolver(address: IPAddress(fromString: "114.114.114.114")!, port: NEKit.Port(port: 53))
-                dnsServer.registerResolver(resolver)
-                self.interface.register(stack: dnsServer)
-                
-                DNSServer.currentServer = dnsServer
-                
-                let udpStack = UDPDirectStack()
-                self.interface.register(stack: udpStack)
-                let tcpStack = TCPStack.stack
-                tcpStack.proxyServer = self.proxyServer
-                self.interface.register(stack:tcpStack)
-                self.interface.start()
-            }
-            self.started = true
-            
-        }
-        
+        return proxySettings
     }
     
-    
-    override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
-        if enablePacketProcessing {
-            interface.stop()
-            interface = nil
-            DNSServer.currentServer = nil
+    func networkSettings(from config: [String : Any]) -> NETunnelNetworkSettings? {
+
+        guard let ssConfig = check(config: config) else {
+            NSLog("[ERROR] YAML Is Nil")
+            return nil;
         }
         
-        if(proxyServer != nil){
-            proxyServer.stop()
-            proxyServer = nil
-            RawSocketFactory.TunnelProvider = nil
-        }
-        completionHandler()
+        setCurrentRules(ssConfig)
         
-        exit(EXIT_SUCCESS)
+        // the `tunnelRemoteAddress` is meaningless because we are not creating a tunnel.
+        let networkSettings = NEPacketTunnelNetworkSettings(tunnelRemoteAddress: "8.8.8.8")
+        networkSettings.mtu = 1500
+        
+        let ipv4Settings = NEIPv4Settings(addresses: ["192.169.89.1"], subnetMasks: ["255.255.255.0"])
+        networkSettings.ipv4Settings = ipv4Settings
+        networkSettings.proxySettings = proxySettings()
+        
+        return networkSettings
     }
-    
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if keyPath == "defaultPath" {
-            if self.defaultPath?.status == .satisfied && self.defaultPath != lastPath{
-                if(lastPath == nil){
-                    lastPath = self.defaultPath
-                }else{
-                    NSLog("received network change notifcation")
-                    let delayTime = DispatchTime.now() + Double(Int64(1 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
-                    DispatchQueue.main.asyncAfter(deadline: delayTime) {
-                        self.startTunnel(options: nil){_ in}
-                    }
-                }
-            }else{
-                lastPath = defaultPath
-            }
-        }
-        
-    }
-    
-}
 
-
-
-extension PacketTunnelProvider {
-    
 }
