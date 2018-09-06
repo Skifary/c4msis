70fbb01ff704d2f6cf5002344147f854ece1f2b5
Skifary <376512563@qq.com>
2018-09-07 01:59:59 +0800

    remove

--- changeGitInfo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-git filter-branch -f --env-filter "GIT_AUTHOR_NAME='skifary'; GIT_AUTHOR_EMAIL='gskifary@outlook.com'; GIT_COMMITTER_NAME='Skifary'; GIT_COMMITTER_EMAIL='376512563@qq.com';" HEAD
